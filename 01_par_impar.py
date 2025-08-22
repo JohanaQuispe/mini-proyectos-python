981441c96618e33a6d45f2a9cc5469ae58362460
Johana Quispe Ruiz <carolinaquispe2740@gmail.com>
2025-08-21 20:12:51 -0500

    Convertí el ejercicio en un juego de par o impar

---
+++ 01_par_impar.py
@@ -0,0 +1,23 @@
+# Juego: Adivina si es par o impar
+
+import random
+
+print("¡Bienvenido al juego de PAR o IMPAR!")
+print("Escribe 'salir' para terminar el juego.")
+
+while True:
+    numero = random.randint(1, 20)
+    eleccion = input(f"¿El número {numero} es PAR o IMPAR? ").lower()
+
+    if eleccion == "salir":
+        print("Gracias por jugar. ¡Hasta la próxima!")
+        break
+
+    if numero % 2 == 0 and eleccion == "par":
+        print("Correcto, es PAR")
+    elif numero % 2 != 0 and eleccion == "impar":
+        print("Correcto, es IMPAR")
+    else:
+        print("Incorrecto, intenta de nuevo")
+
+    print("-" * 30)
